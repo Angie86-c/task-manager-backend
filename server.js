4d1a06640c1a95b53567a7daa882edf2e8312f63
Angie86-c <mhaladikaone86@gmail.com>
2026-04-29 02:01:56 +0200

    Update CORS for production

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ dotenv.config();
 const { connectDB } = require('./src/config/db');
 
 const app = express();
-
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://frabjous-profiterole-463cab.netlify.app',
   credentials: true,
 }));
 app.use(express.json());
